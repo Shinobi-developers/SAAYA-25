24dd6c68b173688df445b7edbc88a0972b888e29
Naslu kk <naslukk@gmail.com>
2025-02-04 15:02:06 +0530

    [update]: leaderboard score updated

--- src/components/Leaderboard/index.tsx
+++ src/components/Leaderboard/index.tsx
@@ -1,11 +1,12 @@
-  import { FaMedal } from "react-icons/fa";
+  import { useEffect, useState } from "react";
+import { FaMedal } from "react-icons/fa";
+import axios from "axios";
 
 const departmentData = [
-  { name: "Computer Science", points: 0 },
-  { name: "Mechanical Engineering", points: 0 },
-  { name: "Civil Engineering", points: 0 },
-  { name: "Electrical Engineering", points: 0 },
-  { name: "Electronics & Communication", points: 0 },
+  { name: "Gryffindor", branch: "CE & AD", points: 75 },
+  { name: "Slytherin", branch: "CSE" , points: 66 },
+  { name: "Hufflepuff", branch: "ECE , EEE & ME", points: 32 },
+  { name: "Ravenclaw", branch: "MCA & MTECH", points: 0 },
 ];
 
 const getMedalColor = (rank: number) => {
@@ -18,6 +19,23 @@ const getMedalColor = (rank: number) => {
 };
 
 const Leaderboard = () => {
+  const [score, setScore] = useState([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchLeaderboard = async () => {
+    try {
+      const response = await axios.get("https://saaya-25-backend.onrender.com/squad/points");
+      setScore(response.data);
+      console.log();
+    } catch (error) {
+      setError("Failed to load leaderboard");
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+
+    fetchLeaderboard();
+  }, []);
   return (
     <div className="w-full max-w-lg mx-auto bg-white p-10 my-10  md:flex md:flex-col md:items-center">
       <h2 className="text-2xl md:text-4xl font-bold text-gray-900 text-center mb-4">
@@ -35,7 +53,11 @@ const Leaderboard = () => {
               <span className={`text-xl font-bold ${getMedalColor(index + 1)}`}>
                 {index < 3 ? <FaMedal size={24} /> : `#${index + 1}`}
               </span>
+              <div className="flex flex-col">
+
               <p className="md:text-xl text-gray-800 font-medium">{dept.name}</p>
+              <p className="md:text-xl text-gray-500 font-medium">{dept.branch}</p>
+              </div>
             </div>
             <p className="md:text-xl text-gray-700 font-semibold">{dept.points} pts</p>
           </li>
